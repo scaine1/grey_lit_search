af13234fcccbdced26bb8a447643b7829f4fb961
Simon Caine <simon@offshoreweather.com.au>
2019-09-04 21:59:02 +1000

    inital import, google result and google class created

---
+++ grey_lit_search/google.py
@@ -0,0 +1,106 @@
+import logging
+from bs4 import BeautifulSoup as soup
+
+"""
+Copyright 2019 Simon Caine
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+logger = logging.getLogger(__name__)
+
+
+class GoogleResult:
+    """
+    Simple class that will take contents of a <div class="g">
+    """
+
+    def __init__(self, div_g):
+        self.div_g = div_g
+
+    def get_title(self):
+        """
+        Return the title of the search
+        using <h3> to determine this
+        """
+
+        title = self.div_g.find_all("h3")
+        try:
+            title = title[0].text
+        except IndexError:
+            logger.warning("could not get title of search")
+            title = None
+        return title
+
+    def get_links(self):
+        """
+        Return a list of links contained within the results
+        Probably want to filter so we only get pdfs
+        """
+
+        links = self.div_g.find_all("a", href=True)
+        return links
+
+    @property
+    def title(self):
+        return self.get_title()
+
+    @property
+    def pdf_links(self):
+        """
+        There can be multiple links in a search results.
+        e.g. to cached values etc.
+        Want to make sure we only get links to pdfs
+        """
+
+        pdf_links = []
+        for link in self.get_links():
+            if not ".pdf" in link["href"].lower():
+                continue
+            # ignore cached results as they may double up results
+            if "webcache" in link["href"].lower():
+                continue
+            if link.text == "Similar":
+                # skip similar links, they go to other searchers
+                continue
+            # need to filter out query string for downloading
+            qindx = link["href"].find("?")
+            if qindx > 0:
+                pdf_links.append(link["href"][0:qindx])
+            else:
+                pdf_links.append(link["href"])
+
+        return pdf_links
+
+
+def get_search_results(webpage):
+    """
+    Break down the search into indiviual items
+    using <div class="g"
+
+    Later on we will extract title and links from
+    these results
+    """
+
+    soup_page = soup(webpage, "html.parser")
+    results = soup_page.find_all("div", {"class": "g"})
+    logger.info(f"Found {len(results)} results, processing now")
+    for result in results:
+        googleresult = GoogleResult(result)
+        yield googleresult
---
+++ tests/test_google.py
@@ -0,0 +1,73 @@
+import pytest
+from pathlib import Path
+from bs4 import BeautifulSoup as soup
+
+from grey_lit_search.google import GoogleResult, get_search_results
+
+test_dir = Path(__file__).resolve().parent.joinpath("test_data")
+
+
+@pytest.fixture()
+def load_sample_result():
+    with open(test_dir.joinpath("sample_result.html"), "r") as fid:
+        webpage_data = fid.readline()
+    return soup(webpage_data, "html.parser").find("div", {"class": "g"})
+
+
+@pytest.fixture()
+def load_sample_result_with_query():
+    with open(test_dir.joinpath("sample_result_with_query_string.html"), "r") as fid:
+        webpage_data = fid.readline()
+    return soup(webpage_data, "html.parser").find("div", {"class": "g"})
+
+
+@pytest.fixture()
+def load_sample_search():
+    with open(test_dir.joinpath("sample_search.html"), "r") as fid:
+        return "".join(fid.readlines())
+
+
+def test_google_result_title(load_sample_result):
+    result = GoogleResult(load_sample_result)
+    assert result.title == "A Simple PDF File"
+
+
+def test_google_result_links(load_sample_result):
+    result = GoogleResult(load_sample_result)
+    assert len(result.get_links()) == 4
+
+
+def test_google_result_pdf_links(load_sample_result):
+    result = GoogleResult(load_sample_result)
+    assert len(result.pdf_links) == 1
+    assert result.pdf_links[0] == "http://www.africau.edu/images/default/sample.pdf"
+
+
+def test_google_result_pdf_links_remove_query_string(load_sample_result_with_query):
+    result = GoogleResult(load_sample_result_with_query)
+    assert len(result.pdf_links) == 1
+    assert result.pdf_links[0] == "http://www.africau.edu/images/default/sample.pdf"
+
+
+def test_get_number_of_individual_search_results(load_sample_search):
+    search_generator = get_search_results(load_sample_search)
+    assert len(list(search_generator)) == 11
+
+
+def test_individual_search_results(load_sample_search):
+    search_generator = get_search_results(load_sample_search)
+    titles = [result.title for result in search_generator]
+    expected_titles = [
+        "A Simple PDF File",
+        "PDF document - pdf 995",
+        "Dummy PDF file",
+        "PDF Bookmark Sample - Adobe",
+        "Sample .pdf download | File Examples Download",
+        "Sample PDF Document",
+        "This is a test PDF file - UNEC",
+        "Prince - Sample Documents - Prince XML",
+        "PDF Bean Inc. - PDF Samples- Convert Word, Excel, PowerPoint to ...",
+        "PDF Test Page",
+        None,
+    ]
+    assert titles == expected_titles
