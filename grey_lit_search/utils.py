c2d60208c68cd7ea4eb8c6798e060bf510b6dc35
Simon Caine <simon@offshoreweather.com.au>
2019-09-13 00:28:04 +1000

    added util functions, save_links etc

---
+++ grey_lit_search/utils.py
@@ -0,0 +1,93 @@
+import os
+import logging
+
+import requests
+
+"""
+Copyright 2019 Simon Caine
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+logger = logging.getLogger(__name__)
+
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+}
+
+
+def save_link(search_num, link, pdf_dir="pdfs", timeout=60):
+    """
+    given a pdf link download the pdf into a subfolder
+    based on the search number
+    """
+
+    save_dir = os.path.join(pdf_dir, f"{str(search_num).zfill(3)}")
+    os.makedirs(save_dir, exist_ok=True)
+    fname = os.path.join(save_dir, os.path.basename(link))
+    logger.info(f"attempting to download {fname}")
+
+    try:
+        page = requests.get(link, headers=headers, timeout=60)
+        page.raise_for_status()
+        with open(fname, "wb") as fid:
+            fid.write(page.content)
+        logger.info(f"    {fname} saved")
+    except requests.exceptions.HTTPError:
+        logger.error(f"posted link does not exit")
+        write_fail_msg(fname, link)
+    except requests.exceptions.Timeout as e:
+        logger.warning(f"download failed with error {e}")
+        write_timeout_msg(fname, link)
+
+
+def write_timeout_msg(fname, link):
+    """
+    Create a file in liew of the correct one that lets the user
+    know the request timed out and include link so they can manually
+    download the file.
+
+    This can happen if the pdf file is quite large or the internet
+    connection is a bit dodgy
+    """
+
+    os.makedirs(os.path.dirname(fname), exist_ok=True)
+
+    with open(fname + ".timedout.txt", "w") as fid:
+        msg = (
+            "timed out when trying to download,"
+            " please manually download using the link below\n"
+            f"{link}"
+        )
+        fid.writelines(msg)
+
+
+def write_fail_msg(fname, link):
+    """
+    Create a file in liew of the correct one that lets the user
+    know the requested link does not exist
+    """
+
+    os.makedirs(os.path.dirname(fname), exist_ok=True)
+
+    with open(fname + ".404error.txt", "w") as fid:
+        msg = "recieved 404 error when trying to download\n" f"{link}"
+        fid.writelines(msg)
+
---
+++ tests/test_utils.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+import os
+import shutil
+
+import mock
+import pytest
+import requests
+from requests.exceptions import HTTPError
+import grey_lit_search.utils as utl
+
+test_dir = Path(__file__).resolve().parent.joinpath("test_data")
+
+
+@pytest.fixture
+def setup():
+    # setup
+    yield
+    # teardown
+    if os.path.isdir("tests/test_output"):
+        shutil.rmtree("tests/test_output")
+
+
+def test_write_timeout_message_file_is_created(setup):
+    fname = "tests/test_output/pdfs/000/fake.pdf"
+    link = "https:fakesite.com/fake.pdf"
+    utl.write_timeout_msg(fname, link)
+    assert os.path.isfile(f"{fname}.timedout.txt")
+
+
+def test_write_timeout_message_msg_is_correct(setup):
+    fname = "tests/test_output/pdfs/000/fake.pdf"
+    link = "https:fakesite.com/fake.pdf"
+    utl.write_timeout_msg(fname, link)
+    with open(f"{fname}.timedout.txt", "r") as fid:
+        lines = fid.readlines()
+    assert lines[0] == (
+        "timed out when trying to download,"
+        " please manually download using the link below\n"
+    )
+    assert lines[1] == link
+
+
+def test_write_fail_message_file_is_created(setup):
+    fname = "tests/test_output/pdfs/000/fail.pdf"
+    link = "https:failsite.com/fail.pdf"
+    utl.write_fail_msg(fname, link)
+    assert os.path.isfile(f"{fname}.404error.txt")
+
+
+def test_write_fail_message_msg_is_correct(setup):
+    fname = "tests/test_output/pdfs/000/fail.pdf"
+    link = "https:failsite.com/fail.pdf"
+    utl.write_fail_msg(fname, link)
+    with open(f"{fname}.404error.txt", "r") as fid:
+        lines = fid.readlines()
+    assert lines[0] == ("recieved 404 error when trying to download\n")
+    assert lines[1] == link
+
+
+@mock.patch("grey_lit_search.utils.requests.get")
+def test_save_link_saves_pdf(mock_requests, setup):
+    # mock_requests().status_code.return_value = 200
+    mock_requests().raise_for_status.return_value = None
+    mock_requests().content = b"fake byte data"
+    link = "http://fakesite.com/fake.pdf"
+    utl.save_link(0, link, pdf_dir="tests/test_output/pdfs")
+    expected_file = "tests/test_output/pdfs/000/fake.pdf"
+    assert os.path.isfile(expected_file)
+    with open(expected_file, "rb") as fid:
+        assert fid.readline() == b"fake byte data"
+
+
+@mock.patch("grey_lit_search.utils.requests.get")
+def test_save_link_captures_httperror(mock_requests, setup):
+    mock_resp = requests.models.Response()
+    mock_resp.status_code = 404
+    mock_requests.return_value = mock_resp
+    link = "http://fakesite.com/fake.pdf"
+    utl.save_link(0, link, pdf_dir="tests/test_output/pdfs")
+    expected_file = "tests/test_output/pdfs/000/fake.pdf.404error.txt"
+    assert os.path.isfile(expected_file)
+    with open(expected_file, "r") as fid:
+        lines = fid.readlines()
+    assert lines[0] == ("recieved 404 error when trying to download\n")
+    assert lines[1] == link
+
+
+@mock.patch("grey_lit_search.utils.requests.get")
+def test_save_link_captures_timeouterror(mock_requests, setup):
+    mock_requests.side_effect = requests.exceptions.Timeout
+    link = "http://fakesite.com/fake.pdf"
+    utl.save_link(0, link, pdf_dir="tests/test_output/pdfs")
+    expected_file = "tests/test_output/pdfs/000/fake.pdf.timedout.txt"
+    assert os.path.isfile(expected_file)
+    with open(expected_file, "r") as fid:
+        lines = fid.readlines()
+    assert lines[0] == (
+        "timed out when trying to download,"
+        " please manually download using the link below\n"
+    )
+    assert lines[1] == link
