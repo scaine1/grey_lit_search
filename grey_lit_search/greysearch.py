1670e4aa32f47de59b943f555cb4bf936fea3ef2
Simon Caine <simon@offshoreweather.com.au>
2019-12-02 21:20:16 +1100

    ran black on greysearch.py
    version bump

--- grey_lit_search/greysearch.py
+++ grey_lit_search/greysearch.py
@@ -7,16 +7,15 @@ import click
 from grey_lit_search import search_and_download
 
 formatter = logging.Formatter(
-    fmt='%(asctime)s: %(levelname)s: %(name)s: %(message)s',
-    datefmt='%Y-%m-%d %H:%M%S'
-    )
+    fmt="%(asctime)s: %(levelname)s: %(name)s: %(message)s", datefmt="%Y-%m-%d %H:%M%S"
+)
 
 handlers = []
 stream_handler = StreamHandler(sys.stdout)
 stream_handler.setFormatter(formatter)
 handlers.append(stream_handler)
 
-file_handler = FileHandler(filename='googlesearch.log')
+file_handler = FileHandler(filename="googlesearch.log")
 file_handler.setFormatter(formatter)
 handlers.append(file_handler)
 
@@ -27,9 +26,10 @@ logger = logging.getLogger(__name__)
 
 @click.command()
 @click.option("--url", "-u", required=True)
-@click.option("--results", "-r", type=int,  default=100)
+@click.option("--results", "-r", type=int, default=100)
 def greysearch(url, results):
     search_and_download(url, results)
+
 
 if __name__ == "__main__":
     greysearch()
